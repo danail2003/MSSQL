f23f9d4f76a690479f27609a798d9c8153d0b2a0
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2020-12-22 20:41:11 +0200

    Add files via upload

---
+++ Built-in-functions/Orders Built-in-functions.sql
@@ -0,0 +1,28 @@
+--Problem 18
+
+SELECT ProductName, OrderDate,
+DATEADD(DAY, 3, OrderDate) AS 'Pay Due',
+DATEADD(MONTH, 1, OrderDate) AS 'Deliver Due'
+FROM Orders
+
+--Problem 19
+
+CREATE TABLE People (
+  Id INT PRIMARY KEY,
+  [Name] NVARCHAR(30) NOT NULL,
+  Birthdate DATETIME NOT NULL
+)
+
+INSERT INTO People (Id, [Name], Birthdate) VALUES
+(1, 'Victor', '2000-12-07 00:00:00.000'),
+(2, 'Steven', '1992-09-10 00:00:00.000'),
+(3, 'Stephen', '1910-09-19 00:00:00.000'),
+(4, 'John', '2010-01-06 00:00:00.000')
+
+SELECT [Name], DATEDIFF(YEAR, Birthdate, GETDATE()) AS 'Age in Years',
+DATEDIFF(MONTH, Birthdate, GETDATE()) AS 'Age in Months',
+DATEDIFF(DAY, Birthdate, GETDATE()) AS 'Age in Days',
+DATEDIFF(MINUTE, Birthdate, GETDATE()) AS 'Age in Minutes'
+FROM People
+
+DROP TABLE People
